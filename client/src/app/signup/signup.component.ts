17e4ec9bf7b3816bcc340b9771dc8f7cea0c6eff
own3mall <own3mall@gmail.com>
2019-03-27 09:53:30 -0600

    Don't Use Explicit

--- client/src/app/signup/signup.component.ts
+++ client/src/app/signup/signup.component.ts
@@ -62,7 +62,7 @@ export class SignupComponent extends FormReactive implements OnInit {
     if(this.recaptchaRequired){
 		var s = document.createElement("script");
 		s.type = "text/javascript";
-		s.src = "https://www.google.com/recaptcha/api.js?render=explicit";
+		s.src = "https://www.google.com/recaptcha/api.js";
 		this.elementRef.nativeElement.appendChild(s);
 	}
   }
